b98232e8056d032c5c122be55d2d3c6b5c338ed7
xihadulislam <xihad.bd@gmail.com>
2021-06-28 21:08:25 +0600

    init 2

--- app/src/main/java/com/xd/androidutils/MainActivity.kt
+++ app/src/main/java/com/xd/androidutils/MainActivity.kt
@@ -2,10 +2,18 @@ package com.xd.androidutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.xihad.myapplication.utils.AndroidUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AndroidUtils.init(this)
+
+        AndroidUtils.sharePrefSettings.setBoolValue("key", false)
+
+        AndroidUtils.toast(AndroidUtils.sharePrefSettings.getBoolValue("key").toString())
+
     }
 }
