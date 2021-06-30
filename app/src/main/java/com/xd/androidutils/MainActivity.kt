cd1b80399c2d0f52187926abf7945fd08af64c7e
xihadulislam <xihad.bd@gmail.com>
2021-06-30 21:57:38 +0600

    uses

--- app/src/main/java/com/xd/androidutils/MainActivity.kt
+++ app/src/main/java/com/xd/androidutils/MainActivity.kt
@@ -16,14 +16,75 @@ class MainActivity : AppCompatActivity() {
 
         AndroidUtils.sharePrefSettings.setBoolValue("key", false)
 
-        AndroidUtils.toast(AndroidUtils.sharePrefSettings.getBoolValue("key").toString())
+        val kBoolean = AndroidUtils.sharePrefSettings.getBoolValue("key")
 
-        findViewById<Button>(R.id.click).setOnClickListener {
-            AndroidUtils.successSnack(root, "dgfdfsg", Gravity.BOTTOM) {
-                AndroidUtils.toast("ggggggggggg")
-            }
+        AndroidUtils.sharePrefSettings.setStringValue("key2", "xihad islam")
 
+        val xd = AndroidUtils.sharePrefSettings.getStringValue("key2");
+        AndroidUtils.toast(xd)
+
+        toast.setOnClickListener {
+            AndroidUtils.toast("show something")
         }
+        showSnack.setOnClickListener {
+            AndroidUtils.snackBar("show something")
+        }
+
+        showSnackSuccess.setOnClickListener {
+            AndroidUtils.successSnack(root, "show something")
+        }
+
+        showSnackInfo.setOnClickListener {
+            AndroidUtils.infoSnack(root, "show something", Gravity.BOTTOM, fun() {
+                AndroidUtils.toast("click")
+            })
+        }
+
+        showSnackWarning.setOnClickListener {
+            AndroidUtils.warningSnack(root, "show something")
+        }
+
+        showSnackError.setOnClickListener {
+            AndroidUtils.errorSnack(root, "show something")
+        }
+
+
+        playTapSound.setOnClickListener {
+            AndroidUtils.playClickSound()
+        }
+
+
+        startMediaPlayer.setOnClickListener {
+          //  AndroidUtils.startMediaPlayer()
+        }
+        stopMediaPlayer.setOnClickListener {
+          //  AndroidUtils.stopMediaPlayer()
+        }
+
+        showKeyboard.setOnClickListener {
+          AndroidUtils.showKeyboard()
+        }
+
+        startNextActivity.setOnClickListener {
+          AndroidUtils.startNextActivity(SecondActivity::class.java)
+        }
+
+        afterNextActivity.setOnClickListener {
+          AndroidUtils.afterNextActivity(2000,SecondActivity::class.java)
+        }
+
+        startFacebookIntent.setOnClickListener {
+          AndroidUtils.startFacebookIntent("url")
+        }
+
+        isInternetAvailable.setOnClickListener {
+          if (AndroidUtils.isInternetAvailable()){
+              AndroidUtils.toast("Available")
+          }
+        }
+
+
+
 
     }
 }
